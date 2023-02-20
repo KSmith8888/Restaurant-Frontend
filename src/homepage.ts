e737a399e24807b42174abb9664552df27fce7a5
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2023-02-19 17:53:28 -0800

    Added backend fetch

--- src/homepage.ts
+++ src/homepage.ts
@@ -7,12 +7,44 @@ const highlightMenuItem = <HTMLDivElement>(
 );
 
 interface MenuItem {
-    Id: number;
-    Name: string;
-    Description: string;
-    Price: number;
-    Alt: string;
-    Highlight: boolean;
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    alt: string;
+    highlight: boolean;
+    path: string;
+}
+
+function createMenuElements(data: MenuItem[]) {
+    data.map((item: MenuItem) => {
+        const menuItemContainer = document.createElement("div");
+        const menuItemImage = document.createElement("img");
+        menuItemImage.src = item.path;
+        menuItemImage.alt = item.alt;
+        menuItemContainer.append(menuItemImage);
+        const menuItemInfo = document.createElement("div");
+        menuItemContainer.append(menuItemInfo);
+        const menuItemName = document.createElement("h3");
+        menuItemName.classList.add("menu-item-name");
+        menuItemName.textContent = `${item.name} - $${item.price.toFixed(2)}`;
+        menuItemInfo.append(menuItemName);
+        const menuItemDescription = document.createElement("p");
+        menuItemDescription.classList.add("menu-item-description");
+        menuItemDescription.textContent = item.description;
+        menuItemInfo.append(menuItemDescription);
+        if (item.highlight) {
+            highlightMenuItem.append(menuItemContainer);
+            menuItemContainer.classList.add("menu-highlight-container");
+            menuItemImage.classList.add("menu-highlight-image");
+            menuItemInfo.classList.add("menu-highlight-info");
+        } else {
+            mainMenuItems.append(menuItemContainer);
+            menuItemContainer.classList.add("menu-item-container");
+            menuItemImage.classList.add("menu-item-image");
+            menuItemInfo.classList.add("menu-item-info");
+        }
+    });
 }
 
 async function getDefaultMenuData() {
@@ -22,39 +54,24 @@ async function getDefaultMenuData() {
             throw new Error(`Status error getting data ${response.status}`);
         }
         const data = await response.json();
-        data.map((item: MenuItem) => {
-            const menuItemContainer = document.createElement("div");
-            const menuItemImage = document.createElement("img");
-            menuItemImage.src = `./assets/images/menu-image-${item.Id}.jpg`;
-            menuItemImage.alt = item.Alt;
-            menuItemContainer.append(menuItemImage);
-            const menuItemInfo = document.createElement("div");
-            menuItemContainer.append(menuItemInfo);
-            const menuItemName = document.createElement("h3");
-            menuItemName.classList.add("menu-item-name");
-            menuItemName.textContent = `${item.Name} - $${item.Price.toFixed(
-                2
-            )}`;
-            menuItemInfo.append(menuItemName);
-            const menuItemDescription = document.createElement("p");
-            menuItemDescription.classList.add("menu-item-description");
-            menuItemDescription.textContent = item.Description;
-            menuItemInfo.append(menuItemDescription);
-            if (item.Highlight) {
-                highlightMenuItem.append(menuItemContainer);
-                menuItemContainer.classList.add("menu-highlight-container");
-                menuItemImage.classList.add("menu-highlight-image");
-                menuItemInfo.classList.add("menu-highlight-info");
-            } else {
-                mainMenuItems.append(menuItemContainer);
-                menuItemContainer.classList.add("menu-item-container");
-                menuItemImage.classList.add("menu-item-image");
-                menuItemInfo.classList.add("menu-item-info");
-            }
-        });
+        createMenuElements(data);
     } catch (err) {
         console.error(err);
     }
 }
 
-getDefaultMenuData();
+async function getDynamicMenuData() {
+    try {
+        const response = await fetch("http://127.0.0.1:3000/menu");
+        if (!response.ok) {
+            throw new Error(`Status error getting data ${response.status}`);
+        }
+        const data = await response.json();
+        createMenuElements(data);
+    } catch (err) {
+        console.error(err);
+        getDefaultMenuData();
+    }
+}
+
+getDynamicMenuData();
